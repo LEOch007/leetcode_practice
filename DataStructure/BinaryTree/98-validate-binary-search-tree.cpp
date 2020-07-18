3e8382de00cf1ac97bc4d451d5523609588fce7d
linjliang <864483116@qq.com>
2020-07-18 17:41:12 +0800

    98-validate-binary-search-tree Done.

--- DataStructure/BinaryTree/98-validate-binary-search-tree.cpp
+++ DataStructure/BinaryTree/98-validate-binary-search-tree.cpp
@@ -53,7 +53,82 @@ struct TreeNode {
 
 class Solution {
 public:
+    bool ans = true;
+
     bool isValidBST(TreeNode* root) {
-        
+        // pre_order_traversal(root);
+        // return ans;
+
+        vector<int> result = {};
+        mid_order_traversal(root, result);
+        for (int i = 0; i < (int)result.size()-1; ++i) {
+            if (result[i]>=result[i+1]) { ans = false; break; }
+        }
+        return ans;
+    }
+
+    vector<int> pre_order_traversal(TreeNode* curr){
+        vector<int> range;
+        if (curr == nullptr) return range;
+
+        range.push_back(curr->val);     // min-value
+        range.push_back(curr->val);     // max-value
+
+        if (curr->left != nullptr){
+            vector<int> left_range = pre_order_traversal(curr->left);
+            if (left_range[0]<range[0]) range[0] = left_range[0];
+            if (left_range[1]>range[1]) range[1] = left_range[1];
+
+            // judgement
+            if (left_range[1] >= curr->val) ans = false;
+        }
+
+        if (curr->right != nullptr){
+            vector<int> right_range = pre_order_traversal(curr->right);
+            if (right_range[0]<range[0]) range[0] = right_range[0];
+            if (right_range[1]>range[1]) range[1] = right_range[1];
+
+            // judgement
+            if (right_range[0] <= curr->val) ans = false;
+        }
+
+        return range;
+    }
+
+    void mid_order_traversal(TreeNode* curr, vector<int>& result){
+        if (curr == nullptr) return ;
+
+        mid_order_traversal(curr->left, result);
+        result.push_back(curr->val);
+        mid_order_traversal(curr->right, result);
     }
 };
+
+int main(){
+    int nums[] = {10,5,15,-1,-1,6,20};
+    int size = 7;
+    TreeNode* pnums[size];
+    for (int i = 0; i < size; ++i) {
+        if (nums[i] == -1) { pnums[i] = nullptr; }
+        else{ pnums[i] = new TreeNode(nums[i]); }
+    }
+    for (int i = 0; i < size; ++i) {
+        if (2*i+1<size) pnums[i]->left = pnums[2*i+1];
+        if (2*i+2<size) pnums[i]->right = pnums[2*i+2];
+    }
+
+    Solution s;
+    s.isValidBST(nullptr);
+    cout<<s.ans<<endl;
+}
+
+/*
+ * Note:
+ *
+ * Divide and Conquer, needs to store the range from min-value to max-value
+ *
+ * Or
+ *
+ * Using mid_order_traversal, aka, Inorder traversal to obtain an increasing list
+ * ( When using .size(), always remember it is unsigned int, try to avoid overflow problem )
+ */
