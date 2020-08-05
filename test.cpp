e9ba6b7fd4b2ca4fd3f2e71f41810d118509de06
linjliang <864483116@qq.com>
2020-08-05 21:06:11 +0800

    QuickSort & MergeSort Done.

--- test.cpp
+++ test.cpp
@@ -4,14 +4,18 @@
 
 #include <iostream>
 #include <vector>
+#include <list>
 #include <zconf.h>
 
 using namespace std;
 
 int main(){
     vector<int> v = {3,4,5,6};
-    vector<int> temp (v.rbegin(),v.rend());
+    list<int> lv (v.begin(),v.end());
 
-    for(auto num:temp) cout<<num<<" ";
+    for(auto num:lv) cout<<num<<" ";
     cout<<endl;
+
+    cout<<"size: "<<lv.size()<<endl;
+
 }
